88a6f2a35d1711c60f3b832f4c24225baff524f2
Ross Addison <rossaddison@users.noreply.github.com>
2019-12-10 13:46:53 +0000

    Update index.php
    Missing pagination's page-link class on permissions/index using yii/grid/gridview.  I would prefer to use kartik/grid/gridview as this resolves the problem but I am trying to stick as close as possible to the original code above. My page link class is being overwritten.

--- views/permission/index.php
+++ views/permission/index.php
@@ -20,6 +20,16 @@ $this->breadcrumbs[] = $this->title;
 
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
+    'pager' => [
+            'firstPageLabel' => '<span class="page-link"><i class ="fa fa-chevron-left"></i></span>',
+            'lastPageLabel' => '<span class="page-link"><i class ="fa fa-chevron-right"></i></span>',
+            'prevPageLabel' => '<span class="page-link">Previous</span>',
+            'nextPageLabel' => '<span class="page-link">Next</span>',
+            'pageCssClass'=>'btn btn-light',
+            'activePageCssClass' => 'active',  
+            'maxButtonCount'=> 5,
+            'options'=> ['class'=> 'pagination'], 
+    ],
     'columns' => [
         [
             'class' => 'yii\grid\SerialColumn',
