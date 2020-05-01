25a4c9d2f8384fb6656ea332661702c5f81aae14
pasquale75 <pellicanipasquale@gmail.com>
2020-05-01 12:36:36 +0200

    except on rules for new-pw scenario (password change issue fixed)

--- models/User.php
+++ models/User.php
@@ -92,7 +92,9 @@ class User extends ActiveRecord implements IdentityInterface
             ['name', 'trim'],
             ['name', 'required'],
             ['name', 'unique', 'targetClass' => '\sjaakp\pluto\models\User',
-                'message' => Yii::t('pluto', 'This name has already been taken')],
+                'message' => Yii::t('pluto', 'This name has already been taken'),
+                'except' => [self::NEW_PW]
+            ],
             ['name', 'string', 'min' => 2, 'max' => 60],
 
             ['email', 'trim'],
@@ -100,7 +102,9 @@ class User extends ActiveRecord implements IdentityInterface
             ['email', 'email'],
             ['email', 'string', 'max' => 128],
             ['email', 'unique', 'targetClass' => '\sjaakp\pluto\models\User',
-                'message' => Yii::t('pluto', 'This email address has already been taken')],
+                'message' => Yii::t('pluto', 'This email address has already been taken'),
+                'except' => [self::NEW_PW]
+            ],
 
             ['password', 'required', 'on' => ['create', self::NEW_PW]],
             ['password', 'match', 'pattern' => $mod->passwordRegexp, 'on' => ['create', 'update', self::NEW_PW]],
